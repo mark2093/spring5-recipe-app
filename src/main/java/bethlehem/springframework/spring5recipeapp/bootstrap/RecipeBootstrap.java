db5a2adec060d77d95b2c0271ef1b2c205333c0e
Mark Dsouza <markdsouza93@gmail.com>
2020-08-07 21:53:48 +0530

    files pushed

--- src/main/java/bethlehem/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/bethlehem/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -4,7 +4,9 @@ import bethlehem.springframework.spring5recipeapp.domain.*;
 import bethlehem.springframework.spring5recipeapp.repositories.CategoryRepository;
 import bethlehem.springframework.spring5recipeapp.repositories.RecipeRepository;
 import bethlehem.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import bethlehem.springframework.spring5recipeapp.repositories.reactive.UnitOfMeasureReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -24,6 +26,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    UnitOfMeasureReactiveRepository reactiveRepository;
+
     public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
         this.recipeRepository = recipeRepository;
@@ -33,8 +38,14 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
+//        loadCategories();
+//        loadUom();
         recipeRepository.saveAll(getRecipes());
         log.debug("Loading Bootstrap Data");
+
+
+        log.error("#######");
+        log.error("Count: " + reactiveRepository.count());
     }
 
     private List<Recipe> getRecipes() {
